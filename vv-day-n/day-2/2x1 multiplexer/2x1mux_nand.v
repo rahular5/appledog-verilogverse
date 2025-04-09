f2bc9051382223a1023abbc0b6b82f36f29ffd8e
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-10 00:52:14 +0530

    Update 2x1mux_nand.v

--- vv-day-n/day-2/2x1 multiplexer/2x1mux_nand.v
+++ vv-day-n/day-2/2x1 multiplexer/2x1mux_nand.v
@@ -1,1 +1,14 @@
+// 2x1 mux using nand primitive only (gate level modelling)
 
+module mux2_1 (I0, I1, S, Y);
+  
+  input I0, I1, S;
+  output Y;
+  
+  wire w1, w2, w3;
+  nand n1 (w1, s);
+  nand n2 (w3, S, I1);
+  nand n3 (w2, w1, I0);
+  nand n4 (Y, w2, w3);
+  
+endmodule
