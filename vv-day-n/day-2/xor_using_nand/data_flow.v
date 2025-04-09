2d225cf2a142d3910339607f00f1962e3959baa2
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-10 00:40:47 +0530

    Update data_flow.v

--- vv-day-n/day-2/xor_using_nand/data_flow.v
+++ vv-day-n/day-2/xor_using_nand/data_flow.v
@@ -1,1 +1,11 @@
+// 2 input xor gate using nand gate only (data flow modelling)
 
+module xor_using_nand_df(A,B,Y);
+  input A, B;
+  output Y;
+  wire w1, w2, w3;
+  assign w1 = ~(A & B);
+  assign w2 = ~(A & w1);
+  assign w3 = ~(B & w1);
+  assign Y = ~(w2 & w3);
+endmodule
