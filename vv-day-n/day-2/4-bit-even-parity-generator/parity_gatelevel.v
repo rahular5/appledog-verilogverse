6933158f8de8736fa2712574544f356480f67cbf
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-10 01:03:26 +0530

    Update parity_gatelevel.v

--- vv-day-n/day-2/4-bit-even-parity-generator/parity_gatelevel.v
+++ vv-day-n/day-2/4-bit-even-parity-generator/parity_gatelevel.v
@@ -1,1 +1,11 @@
+// 4 bit even parity generator circuit (gate level modelling)
 
+module even_parity(b0, b1, b2, b3, P);
+  
+  input b0, b1, b2, b3;
+  output P;
+  
+  xor x1 (P, b0, b1, b2, b3);
+  
+endmodule
+  
