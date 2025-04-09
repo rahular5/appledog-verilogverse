24155b4bea87b453398efae3f4ac950f377bdc6c
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-10 00:16:56 +0530

    Update mux2_1.v

--- vv-day-n/day-2/2x1 multiplexer/mux2_1.v
+++ vv-day-n/day-2/2x1 multiplexer/mux2_1.v
@@ -1,32 +1,11 @@
-// testbench for 2x1 mux
+// Code for 2x1 mux
 
-
-module mux2_1_tb;
+module mux2_1(I0,I1,s,y);
+  input I0,I1,s;
+  output y;
   
-  reg I0,I1,s;
-  wire y;
-  mux2_1 dut(I0,I1,s,y);
-  
-  initial begin
-    I0 = 1'b0; I1 = 1'b0; s = 1'b0;
-    #5 I0 = 1'b0; I1 = 1'b0; s = 1'b1;
-    #5 I0 = 1'b0; I1 = 1'b1; s = 1'b0;
-    #5 I0 = 1'b0; I1 = 1'b1; s = 1'b1;
-    #5 I0 = 1'b1; I1 = 1'b0; s = 1'b0;
-    #5 I0 = 1'b1; I1 = 1'b0; s = 1'b1;
-    #5 I0 = 1'b1; I1 = 1'b1; s = 1'b0;
-    #5 I0 = 1'b1; I1 = 1'b1; s = 1'b1;
-  end
-  
-  initial begin
-    $monitor("simulationTime = %0t, I0 = %0b, I1 = %0b, s = %0b, y = %0b",$time,I0,I1,s,y);
-    
-  end
-  
-  initial begin
-    $dumfile("dump.vcd");
-    $dumpvars(0,mux2_1_tb);
-  end
-  
+  assign y = ( (~I0) & s)|(s & I1);
 endmodule
+
+
 
