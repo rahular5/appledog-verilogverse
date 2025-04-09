c41b6afbbdb4486851dc823a477a7ca6475c4e90
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-10 01:05:37 +0530

    Update parity_df.v

--- vv-day-n/day-2/4-bit-even-parity-generator/parity_df.v
+++ vv-day-n/day-2/4-bit-even-parity-generator/parity_df.v
@@ -1,1 +1,11 @@
+// 4 bit even parity generator circuit (dataflow modelling)
 
+module even_parity_df(b0, b1, b2, b3, P);
+  
+  input b0, b1, b2, b3;
+  output P;
+  
+  assign P = ( b0 ^ b1 ^ b2 ^ b3 );
+  
+endmodule
+  
