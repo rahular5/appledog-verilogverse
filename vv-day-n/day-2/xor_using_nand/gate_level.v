ef2c30e56691ce548a3bea1e5e70cde9ef0e23a5
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-10 00:37:11 +0530

    Update gate_level.v

--- vv-day-n/day-2/xor_using_nand/gate_level.v
+++ vv-day-n/day-2/xor_using_nand/gate_level.v
@@ -1,1 +1,11 @@
+// 2 input xor gate using nand gate only (gate level modelling)
 
+module xor_using_nand(A,B,Y);
+  input A, B;
+  output Y;
+  wire w1, w2, w3;
+  nand A1 (w1, A, B);
+  nand A2 (w2, A, w1);
+  nand A3 (w3, B, w1);
+  nand A4 (Y, w2 ,w3);
+endmodule
