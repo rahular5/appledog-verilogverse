0a112b5ace5e7584771f4bcf518213791c82690b
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-24 13:43:47 +0530

    Update inc_dec_counter_tb.v

--- vv-day-n/day-6/gradually increment and decrement counter(0 to 7 to 0)/inc_dec_counter_tb.v
+++ vv-day-n/day-6/gradually increment and decrement counter(0 to 7 to 0)/inc_dec_counter_tb.v
@@ -1,1 +1,22 @@
-
+module test;
+  
+  reg clk,rst;
+  wire[2:0]count;
+  
+  counter dut(clk,rst,count);
+  
+  initial begin
+    clk = 0;
+    rst = 1;
+    #13 rst = 0;
+    #1000 $finish;
+  end
+  
+  always #5 clk =~clk;
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,clk,rst,count);
+  end
+  
+endmodule
