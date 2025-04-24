d463fed46d376ed86ce46b91caa42c35f1abf7eb
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-24 13:42:46 +0530

    Update inc_dec_counter.v

--- vv-day-n/day-6/gradually increment and decrement counter(0 to 7 to 0)/inc_dec_counter.v
+++ vv-day-n/day-6/gradually increment and decrement counter(0 to 7 to 0)/inc_dec_counter.v
@@ -1,1 +1,36 @@
+module counter(clk,rst,count);
+  
+  input clk,rst;
+  output [2:0]count;
+  reg [2:0] temp;
+  reg ctrl;
+  
+  assign count = temp;
 
+  
+  always@(posedge clk) begin
+    if(rst) begin
+      temp<=3'b000;
+      ctrl<=1;
+    end
+    else
+      if (ctrl) begin
+        if (temp==3'b111) begin
+          ctrl<=0;
+          temp=temp-1;
+        end
+        else begin
+          temp=temp+1;
+        end
+      end
+    
+    
+    else
+      if(temp==3'b000) begin
+          ctrl<=1;
+          temp<=temp+1;
+        end
+        else
+          temp<=temp-1;
+  end
+endmodule
