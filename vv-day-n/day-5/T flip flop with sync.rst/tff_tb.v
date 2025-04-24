81e0b82994ce5057fcb3510ed2fc286315088031
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-24 07:36:36 +0530

    Update tff_tb.v

--- vv-day-n/day-5/T flip flop with sync.rst/tff_tb.v
+++ vv-day-n/day-5/T flip flop with sync.rst/tff_tb.v
@@ -1,1 +1,32 @@
+// Testbench for  positive edge triggered tff with synchronous reset. 
 
+module tff_test;
+  reg clk,rst,T;
+  wire Q;
+  tff dut(clk,rst,T,Q);
+  initial begin
+    clk = 0;
+    rst = 1;
+    #12 rst = 0;
+    T = 0;
+    #8  T = 0;   // At 20 ns -> no toggle
+    #10 T = 1;   // At 30 ns -> toggle
+    #10 T = 0;   // At 40 ns -> hold
+    #10 T = 1;   // At 50 ns -> toggle
+    #10 T = 0;   // At 60 ns -> hold
+    #10 T = 0;   // At 70 ns -> hold
+    #10 T = 1;   // At 80 ns -> toggle
+    #100 $finish;
+  end
+  
+  always
+    #5 clk <= ~clk;
+  
+  initial begin
+    $dumpfile("Dump.vcd");
+    $dumpvars(0,clk,rst,T,Q);
+  end
+  
+endmodule
+    
+    
