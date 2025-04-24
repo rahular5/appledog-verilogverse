77d73d269e50c05f4640fc90e53ae643b66a2d02
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-24 07:47:52 +0530

    Update tff.v

--- vv-day-n/day-5/T flip flop with async rst/tff.v
+++ vv-day-n/day-5/T flip flop with async rst/tff.v
@@ -1,1 +1,18 @@
+// Verilog code for  positive edge triggered tff with asynchronous reset.
 
+module tff(clk,rst,T,Q);
+  input clk,rst,T;
+  output reg Q;
+  reg temp;
+  
+  always @ (posedge clk or posedge rst) begin
+    if (rst)
+        temp <= 0;
+      else
+        if (T)
+          temp <= ~temp;
+      else
+        temp <= temp;
+    Q <= temp;
+  end
+endmodule
