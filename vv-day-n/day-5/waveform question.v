82855a7e4d6a7b7a4bdf855ed695f8538cd61909
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-24 06:58:37 +0530

    Update waveform question.v

--- vv-day-n/day-5/waveform question.v
+++ vv-day-n/day-5/waveform question.v
@@ -1,1 +1,22 @@
+///To generate the clk1 of 50% duty cycle and another clk2 delayed by half of clk1 duty cycle
+module clk_waveform;
+  reg clk1, clk2;
 
+  initial begin
+    clk1 = 0;
+    clk2 = 0;
+    #100 $finish;
+  end
+
+  always #5 clk1 = ~clk1; 
+
+  always @(clk1)
+    #5 clk2 = clk1;    
+  
+
+  initial begin
+	$dumpfile("dump.vcd");
+    $dumpvars(0,clk1,clk2);
+  end
+
+endmodule
