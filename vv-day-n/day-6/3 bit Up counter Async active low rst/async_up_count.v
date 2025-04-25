77bc4ab1e63833fdb8f503be745c670effa339d9
Rahul Adappa Rabinal <123595167+rahular5@users.noreply.github.com>
2025-04-25 11:14:50 +0530

    Update async_up_count.v

--- vv-day-n/day-6/3 bit Up counter Async active low rst/async_up_count.v
+++ vv-day-n/day-6/3 bit Up counter Async active low rst/async_up_count.v
@@ -1,1 +1,16 @@
+// 3 bit Up counter with Asynchronous active low reset
+module async_low_rst_counter(clk,rst,count);
+  input clk;
+  input rst;         
+  output reg [2:0] count;
+  reg [2:0] temp;
 
+  always @(posedge clk) begin
+    if (!rst)
+      temp <= 3'b000;
+    else
+      temp <= temp + 1;
+  end
+  assign count = temp;
+
+endmodule
